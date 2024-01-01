a73fdb49c0a9bc82eeab770573a6108be144bd30
roniveseli1 <roni.veseli@student.uni-pr.edu>
2024-01-01 16:27:59 +0100

    Added external JavaScript code to create a Slideshow and next slide and previous slide buttons
    Created the code and functions, to create a functioning slideshow and slide changer for the main and first section of the home page, which shows a recent show with its title and director, and switches to the next show throughout a slideshow or buttons

---
+++ resources/js/index.js
@@ -0,0 +1,58 @@
+const shows = [
+    {
+      imageUrl: '../resources/images/aboutUs.jpg',
+      name: '1984',
+      director: "Igor Mendjisky",
+      runningTime: '1:30'
+    },
+    {
+      imageUrl: '../resources/images/aboutUs(2).jpg',
+      name: 'CLUB ALBANIA',
+      director: "Fatos Berisha",
+      runningTime: '2:00'
+    },
+    // Add more objects for other shows
+    ];
+  
+      let currentIndex = 0;
+      const fullPage = document.querySelector('.full-page');
+      const showNameElement = document.getElementById('showName');
+      const directorElement = document.getElementById('director');
+      const runningTimeElement = document.getElementById('runningTime');
+  
+      function changeBackground(image) {
+        fullPage.style.backgroundImage = `url('${image}')`;
+      }
+  
+      function displayShow(index) {
+        const show = shows[index];
+        changeBackground(show.imageUrl);
+        showNameElement.textContent = show.name;
+        directorElement.textContent = `Director: ${show.director}`;
+        runningTimeElement.textContent = `Running Time: ${show.runningTime}`;
+      }
+  
+      function startSlideshow() {
+        setInterval(() => {
+          currentIndex = (currentIndex + 1) % shows.length;
+          displayShow(currentIndex);
+        }, 5000); // Change shows every 5 seconds (5000 milliseconds)
+      }
+  
+      function nextSlide() {
+        currentIndex = (currentIndex + 1) % shows.length;
+        displayShow(currentIndex);
+      }
+  
+      function prevSlide() {
+        currentIndex = (currentIndex - 1 + shows.length) % shows.length;
+        displayShow(currentIndex);
+      }
+  
+      startSlideshow(); // Start the slideshow when the page loads
+  
+      const leftButton = document.querySelector('.hover-button.left');
+      const rightButton = document.querySelector('.hover-button.right');
+  
+      leftButton.addEventListener('click', prevSlide);
+      rightButton.addEventListener('click', nextSlide);

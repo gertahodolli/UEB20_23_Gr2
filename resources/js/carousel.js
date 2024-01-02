df7c5f82e2a96d671d2cdc99e1f7b4624d2b157c
EreaBlakaj <120092128+EreaBlakaj@users.noreply.github.com>
2024-01-02 21:56:01 +0100

    Create carousel.js
    script file for carousel

---
+++ resources/js/carousel.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dots");
+
+            if (n > slides.length) { slideIndex = 1; }
+            if (n < 1) { slideIndex = slides.length; }
+
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" enable", "");
+            }
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " enable";
+        }
+        setInterval(function() {
+            plusSlides(1);
+        }, 2000);

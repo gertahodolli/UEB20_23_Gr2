75d77caf2738670a28a2988e55f022d0a1ba0f36
Lyra Bullaku <117154061+Lyra8@users.noreply.github.com>
2024-01-07 20:39:15 +0100

    updated footer

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,11 +1,11 @@
 
 function startLiveChat() {
-        document.getElementById("liveChatSection").style.display = "block";
-    }
+    document.getElementById("liveChatSection").style.display = "block";
+}
 
-    // Function to open the popup
+// Function to open the popup
 function openPopup() {
-    document.getElementById("liveChatPopup").style.display = "block";
+document.getElementById("liveChatPopup").style.display = "block";
 
 }
 
@@ -13,127 +13,114 @@ function openPopup() {
 
 // Function to close the popup
 function closePopup() {
-    document.getElementById("liveChatPopup").style.display = "none";
+document.getElementById("liveChatPopup").style.display = "none";
 }
 
 // Function to submit the live chat 
 function submitLiveChat() {
-    const name = document.getElementById("liveChatName").value.trim();
-    const email = document.getElementById("liveChatEmail").value.trim();
-    const message = document.getElementById("liveChatMessage").value.trim();
+const name = document.getElementById("liveChatName").value.trim();
+const email = document.getElementById("liveChatEmail").value.trim();
+const message = document.getElementById("liveChatMessage").value.trim();
 
-    // Validimi i emrit, email-it dhe mesazhit
-    const isNameValid = validateName();
-    const isEmailValid = validateEmail();
-    const isMessageValid = message !== "";
+// Validimi i emrit, email-it dhe mesazhit
+const isNameValid = validateName();
+const isEmailValid = validateEmail();
+const isMessageValid = message !== "";
 
-   
-    // Kontrolloni nëse të gjitha fushat janë valide
-    if (isNameValid && isEmailValid && isMessageValid) {
-        alert("Your message has been successfully submitted!");
-        document.getElementById("liveChatSection").style.display = "none";
-        closePopup();
-    } 
-    else {
-        alert("Please fill out all required fields correctly.");
-    }
+
+// Kontrolloni nëse të gjitha fushat janë valide
+if (isNameValid && isEmailValid && isMessageValid) {
+    alert("Your message has been successfully submitted!");
+    document.getElementById("liveChatSection").style.display = "none";
+    closePopup();
+} 
+else {
+    alert("Please fill out all required fields correctly.");
+}
 }
 
 
 
 function validateName() {
-    var name = document.getElementById('liveChatName').value;
-    var nameFormatHint = document.getElementById('nameFormatHint');
+var name = document.getElementById('liveChatName').value;
+var nameFormatHint = document.getElementById('nameFormatHint');
 
-    // Kontrolloni nëse emri është i zbrazët
-    if (name.trim() === '') {
-        nameFormatHint.style.display = 'none';
-        return false;
-    }
+// Kontrolloni nëse emri është i zbrazët
+if (name.trim() === '') {
+    nameFormatHint.style.display = 'none';
+    return false;
+}
 
-    // Kontrolloni për praninë e numrave në emër
-    var containsNumbers = /\d/.test(name);
+// Kontrolloni për praninë e numrave në emër
+var containsNumbers = /\d/.test(name);
 
-    // Shfaqni ose fshehni udhëzimin për formatin e duhur të emrit
-    if (containsNumbers) {
-        nameFormatHint.style.display = 'block';
-    } else {
-        nameFormatHint.style.display = 'none';
-    }
+// shikon se a eshte emri ne formatin e duhur
+if (containsNumbers) {
+    nameFormatHint.style.display = 'block';
+} else {
+    nameFormatHint.style.display = 'none';
+}
 
-    return !containsNumbers;
+return !containsNumbers;
 }
 
 // Function to validate email
 function validateEmail() {
-    var email = document.getElementById('liveChatEmail').value;
-    var emailFormatHint = document.getElementById('emailFormatHint');
+var email = document.getElementById('liveChatEmail').value;
+var emailFormatHint = document.getElementById('emailFormatHint');
 
-    // Kontrolloni nëse email është i zbrazët
-    if (email.trim() === '') {
-        emailFormatHint.style.display = 'none';
-        return false;
-    }
+// Kontrollon nëse email është i zbrazët
+if (email.trim() === '') {
+    emailFormatHint.style.display = 'none';
+    return false;
+}
 
-    // Kontrolloni për një format të vlefshëm të email-it
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var isValidEmail = emailRegex.test(email);
+// Shikon për një format të vlefshëm të email-it
+var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+var isValidEmail = emailRegex.test(email);
 
-    // Shfaqni ose fshehni udhëzimin për formatin e duhur të email-it
-    if (!isValidEmail) {
-        emailFormatHint.style.display = 'block';
-    } else {
-        emailFormatHint.style.display = 'none';
-    }
+if (!isValidEmail) {
+    emailFormatHint.style.display = 'block';
+} else {
+    emailFormatHint.style.display = 'none';
+}
 
-    return isValidEmail;
+return isValidEmail;
 }
 
 function validateMessage() {
-    var message = document.getElementById('liveChatMessage').value;
-    var messageFormatHint = document.getElementById('messageFormatHint');
+var message = document.getElementById('liveChatMessage').value;
+var messageFormatHint = document.getElementById('messageFormatHint');
 
-    // Kontrolloni nëse mesazhi është i zbrazët
-    if (message.trim() === '') {
-        messageFormatHint.style.display = 'block';
-        return false;
-    } else {
-        messageFormatHint.style.display = 'none';
-        return true;
-    }
+// shikon nëse mesazhi është i zbrazët
+if (message.trim() === '') {
+    messageFormatHint.style.display = 'block';
+    return false;
+} else {
+    messageFormatHint.style.display = 'none';
+    return true;
+}
 }
 
+// footeri 
 
-
-// Përditësojeni funksionin për animacionin e emrit
-function animateNameHint() {
-    $("#nameFormatHint").show("slide", { direction: "right" }, 1000);
+// Function to update current date and time
+function updateDateTime() {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString();
+    const formattedTime = currentDate.toLocaleTimeString();
+    const dateTimeString = `Current Date and Time: ${formattedDate} ${formattedTime}`;
+    
+    // Update the content of the element with id "currentDateTime"
+    document.getElementById("currentDateTime").textContent = dateTimeString;
 }
 
-// Përditësojeni funksionin për animacionin e email-it
-function animateEmailHint() {
-    $("#emailFormatHint").show("slide", { direction: "right" }, 1000);
-}
+// Call the function to update date and time initially
+updateDateTime();
 
-// Përditësojeni funksionin për animacionin e mesazhit
-function animateMessageHint() {
-    $("#messageFormatHint").show("slide", { direction: "right" }, 1000);
-}
+// Update date and time every second
+setInterval(updateDateTime, 1000);
 
-// Përditësojeni funksionin për fshehjen e animacionit të emrit
-function hideNameHint() {
-    $("#nameFormatHint").hide("slide", { direction: "left" }, 1000);
-}
-
-// Përditësojeni funksionin për fshehjen e animacionit të email-it
-function hideEmailHint() {
-    $("#emailFormatHint").hide("slide", { direction: "left" }, 1000);
-}
-
-// Përditësojeni funksionin për fshehjen e animacionit të mesazhit
-function hideMessageHint() {
-    $("#messageFormatHint").hide("slide", { direction: "left" }, 1000);
-}
 
 
 

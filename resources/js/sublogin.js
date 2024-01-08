79c0eb4d3230d293775378ea8393236b6efff202
Gerta Hodolli <gerta.hodolli@student.uni-pr.edu>
2024-01-08 18:08:42 +0100

    Upgrade Form Validation

--- resources/js/sublogin.js
+++ resources/js/sublogin.js
@@ -1,14 +1,39 @@
-function ValidateEmail(inputText)
-{
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-return true;
+function ValidateEmail(inputText){
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(inputText.value.match(mailformat))
+    {
+        alert("Valid email address!");
+        return true;
+    }
+    else
+    {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
 }
-else
-{
-alert("You have entered an invalid email address!");
-return false;
+
+function ValidatePassword(password) {
+    var passwordFormat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    
+    if (password.value.match(passwordFormat)) {
+        alert("Valid password!");
+        return true;
+    } else {
+        alert("Password should be at least 8 characters long and include a combination of lowercase, uppercase, and numeric characters.");
+        return false;
+    }
 }
+
+function ValidateUsername(username) {
+    var usernameFormat = /^.{4,20}$/;
+    
+    if (username.value.match(usernameFormat)) {
+        alert("Valid username!");
+        return true;
+    } else {
+        alert("Username must be between 4 and 20 characters.");
+        return false;
+    }
 }
+
+
